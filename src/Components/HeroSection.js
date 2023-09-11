e4a8673b2368324d20c81bfea7af85670190a9f4
Semilogo Bamidele <emmanbamidele471@gmail.com>
2023-09-11 07:54:57 +0100

    project setup completed

---
+++ src/Components/HeroSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Poster from '../Assets/Poster.svg'
+
+export const HeroSection = () => {
+  return (
+      <div>
+        <img src={Poster} alt="posterImage"  className="w-full" />
+    </div>
+  )
+}
